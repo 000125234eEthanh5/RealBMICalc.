4809e811721f3500e46c222f3daaa6f8ecd62fb6
000125234eEthanh5 <32342571+000125234eEthanh5@users.noreply.github.com>
2017-10-20 12:28:58 -0600

    bmi final

--- ViewController.cs
+++ ViewController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using UIKit;
 
 namespace RealBMICalc
@@ -15,12 +14,31 @@ namespace RealBMICalc
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+
+            CalculateButton.TouchUpInside+= CalculateButton_TouchUpInside;
+        }
+
+        void CalculateButton_TouchUpInside(object sender, EventArgs e)
+        {
+            float height = float.Parse(HeightTextField.Text);
+            float weight = float.Parse(WeightTextField.Text);
+
+            float bmi = (weight / (height * height));
+
+            BMILable.Text = bmi.ToString();
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void TouchesBegan(Foundation.NSSet touches, UIEvent evt)
+        {
+            base.TouchesBegan(touches, evt);
+
+            this.View.EndEditing (true);
         }
     }
 }
--- ViewController.designer.cs
+++ ViewController.designer.cs
@@ -1,17 +1,55 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace RealBMICalc
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel BMILable { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CalculateButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField HeightTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField WeightTextField { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (BMILable != null) {
+                BMILable.Dispose ();
+                BMILable = null;
+            }
+
+            if (CalculateButton != null) {
+                CalculateButton.Dispose ();
+                CalculateButton = null;
+            }
+
+            if (HeightTextField != null) {
+                HeightTextField.Dispose ();
+                HeightTextField = null;
+            }
+
+            if (WeightTextField != null) {
+                WeightTextField.Dispose ();
+                WeightTextField = null;
+            }
         }
     }
 }
